1b73a380c3cf6d7a79c06925f695dc4d62bb8076
Amirhossein Azizi <amirhosseinazizi10@gmail.com>
2023-09-23 14:40:41 +0330

    merged file into one

--- temp.cpp
+++ temp.cpp
@@ -1,10 +1,80 @@
+%%writefile sample.cu
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <stdio.h>
-#include "graph_io.h"
+
 #define INF 1000000
 
 using namespace std;
+
+
+/**
+ * Reads a graph from a file and stores its edges in three arrays: u, v, and w.
+ * 
+ * @param filename The name of the file to read the graph from.
+ * @param u A pointer to an integer array that will store the source vertices of the edges.
+ * @param v A pointer to an integer array that will store the destination vertices of the edges.
+ * @param w A pointer to an integer array that will store the weights of the edges.
+ * @param edges_count A reference to an integer that will store the number of edges in the graph.
+ * @param vertices_count A reference to an integer that will store the number of vertices in the graph.
+ */
+void ReadGraphFromFile(const char *filename, vector<int> &u, vector<int> &v, vector<int> &w, int &edges_count, int &vertices_count)
+{
+    
+    // Open the input file
+    std::ifstream file(filename);
+    // Declare a string variable to store each line of the file
+    string line;
+    
+    // Check if the file was opened successfully
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return;
+    }
+    
+    // Declare variables to store the source vertex, destination vertex, and weight of each edge
+    int source, dest, weight;
+    // Declare a character variable to store the first character of each line
+    char c;
+
+    // Declare a counter variable to keep track of the number of edges read
+    int i = 0;
+    // Read each line of the file
+    while (std::getline(file, line)) {
+        // Create a stringstream object from the line
+        stringstream ss(line);
+
+        // Read the first character of the line
+        ss >> c;
+        // If the first character is not 'a', return from the function (not a edge line)
+        if (c != 'a')
+            continue;
+
+        // Read the source vertex, destination vertex, and weight of the edge
+        ss >> source >> dest >> weight;
+        // Push the source vertex, destination vertex, and weight of the edge to the corresponding arrays
+        u.push_back(source - 1); // Subtract 1 from the source and destination vertices to convert them to 0-based indexing
+        v.push_back(dest - 1);
+        w.push_back(weight);
+        // Increment the counter variable
+        i++;   
+    }
+
+    // Store the number of edges read in the edges_count variable
+    edges_count = i;
+    // Store the number of vertices in the graph in the vertices_count variable
+    vertices_count = max(*max_element(u.begin(), u.end()), *max_element(v.begin(), v.end())) + 1;
+    
+    // Close the input file
+    file.close();
+}
+
+
 
 /**
  * @brief CUDA kernel for the Floyd-Warshall algorithm.
@@ -60,7 +130,7 @@ void floyd_warshall(int* distance_matrix, int vertices_count, int edges_count) {
 
     // Perform the Floyd-Warshall algorithm for all vertices.
     for (int k = 0; k < vertices_count; k++) {
-        floyd_warshall_kernel<<<numBlocks, threadsPerBlock>>>(d_distance_matrix, vertices_count, edges_count, k);
+      floyd_warshall_kernel<<<numBlocks, threadsPerBlock>>>(d_distance_matrix, vertices_count, edges_count, k);
     }
 
     // Copy the updated distance matrix from device to host.
@@ -104,19 +174,17 @@ void init_distance_matrix(int* &distance_matrix, int vertices_count, int edges_c
 int main() {
     vector<int> u, v, w;
     int edges_count, vertices_count;
-    ReadGraphFromFile("graph.txt", u, v, w, edges_count, vertices_count);
-
+    ReadGraphFromFile("small graph.gr", u, v, w, edges_count, vertices_count);
     int* distance_matrix;
     init_distance_matrix(distance_matrix, vertices_count, edges_count, u.data(), v.data(), w.data());
-
     floyd_warshall(distance_matrix, vertices_count, edges_count);
-
+    int value;
     for(int i = 0; i < vertices_count; i++){
         for(int j = 0; j < vertices_count; j++){
-            cout << distance_matrix[i * vertices_count + j] << " ";
+            value = distance_matrix[i * vertices_count + j];
+            cout << ((value == 1000000) ? "INF\t" : to_string(value) + "\t");
         }
         cout << endl;
     }
-
     return 0;
 }
